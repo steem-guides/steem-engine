fcf307c35e25ce0e9944f6d1e5dae8b1d857f46d
think-in-universe <mstr.raphael@gmail.com>
2019-06-14 17:56:46 +0800

    generate PDF output only if the environment variable is set

--- scripts/_build.sh
+++ scripts/_build.sh
@@ -2,12 +2,17 @@
 
 set -ev
 
-# add the update to fix ctexbook too old issue
-tlmgr update --self --all --reinstall-forcibly-removed
-# preprocess Rmd files before building
-Rscript scripts/bookdown_helper.R
-# build the books
-# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+if [ "${PDF_OUTPUT}" = "true" ]; then
+
+    # add the update to fix ctexbook too old issue
+    tlmgr update --self --all --reinstall-forcibly-removed
+    # preprocess Rmd files before building
+    Rscript scripts/bookdown_helper.R
+    # build the books
+    Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+    Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+
+fi
+
 # make gitbook the last target to add the links to pdf and epub
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
